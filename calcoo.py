e3470eb0f2d1b342a14df650f6aa64bc72caf174
anunezd <alejandro_nd93@hotmail.com>
2018-10-01 16:53:04 +0200

    Cambios con los operadores de las funciones

--- calcoo.py
+++ calcoo.py
@@ -6,23 +6,19 @@ import sys
 
 class Calculadora():
 
-    def __init__(self, op1, op2):
-        self.operador1 = op1
-        self.operador2 = op2
+    def suma(self, op1, op2):
+        return op1 + op2
 
-    def suma(self):
-        return self.operador1 + self.operador2
-
-    def resta(self):
-        return self.operador1 - self.operador2
+    def resta(self, op1, op2):
+        return op1 - op2
 
 
 if __name__ == '__main__':
     operador1 = float(sys.argv[1])
     operador2 = float(sys.argv[3])
     operacion = sys.argv[2]
-    calcula = Calculadora(operador1, operador2)
+    calcula = Calculadora()
     if operacion == 'suma':
-        print(calcula.suma())
+        print(calcula.suma(operador1, operador2))
     elif operacion == 'resta':
-        print(calcula.resta())
+        print(calcula.resta(operador1, operador2))
--- calcoohija.py
+++ calcoohija.py
@@ -7,26 +7,26 @@ import calcoo
 
 class CalculadoraHija(calcoo.Calculadora):
 
-    def multiplicacion(self):
-        return self.operador1 * self.operador2
+    def multiplicacion(self, op1, op2):
+        return op1 * op2
 
-    def division(self):
-        if operador2 == 0:
+    def division(self, op1, op2):
+        if op2 == 0:
             exit('Division by zero is not allowed')
         else:
-            return self.operador1 / self.operador2
+            return op1 / op2
 
 
 if __name__ == '__main__':
     operador1 = float(sys.argv[1])
     operador2 = float(sys.argv[3])
     operacion = sys.argv[2]
-    calcula = CalculadoraHija(operador1, operador2)
+    calcula = CalculadoraHija()
     if operacion == 'suma':
-        print(calcula.suma())
+        print(calcula.suma(operador1, operador2))
     elif operacion == 'resta':
-        print(calcula.resta())
+        print(calcula.resta(operador1, operador2))
     elif operacion == 'multiplica':
-        print(calcula.multiplicacion())
+        print(calcula.multiplicacion(operador1, operador2))
     elif operacion == 'divide':
-        print(calcula.division())
+        print(calcula.division(operador1, operador2))
